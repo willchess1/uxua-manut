36502dd5d39c29824d7beab12475187bb7193ca0
william chesman santana oliveira <wilches1986@gmail.com>
2026-05-14 14:52:45 -0300

    fix: add .env to .gitignore

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://hojhvyinstbrweeggfoe.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvamh2eWluc3RicndlZWdnZm9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMzkwNzQsImV4cCI6MjA5MjgxNTA3NH0.mbAlIW0po10GYl09jCOwoCuLZlcm8AK-6_Rh0xNKPUA'
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
